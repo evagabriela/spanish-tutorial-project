b63b0df598b7b1b5c9f22845bf702978c29dc60e
Gabriela Zamudio <gabriela.osu@gmail.com>
2017-08-19 11:12:05 -0700

    refactor and clean code with latest changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-  $(document).ready( function() {
+
+// Modal 
     if (screen.width <= 480) {
         $('.box-item a').on('click', function (event) {
             event.preventDefault();
@@ -9,4 +10,30 @@
              });
         });
     }
-});
+
+// Subscription Box
+
+    $('.open').on('click', function () {
+        $('body').toggleClass('active');
+         $('.sidebar').show();
+    });
+
+    $('.close').on('click', function () {
+        $('body').removeClass('active');
+      $('.sidebar').hide();
+    });
+
+    
+// classroom
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyBFuC43STFtR0Vx4NYMh8D9R58wJ2IUlCE",
+  //   authDomain: "ekua-e9d9c.firebaseapp.com",
+  //   databaseURL: "https://ekua-e9d9c.firebaseio.com",
+  //   projectId: "ekua-e9d9c",
+  //   storageBucket: "",
+  //   messagingSenderId: "192283074344"
+  // };
+  // firebase.initializeApp(config);
+
+
