e122102b1981e745431e86a511c2fed39491ca93
Cristina Del Puerto <cristidp7@gmail.com>
2017-08-18 17:31:53 -0700

    fix responsive

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@
     if (screen.width <= 480) {
         $('.box-item a').on('click', function (event) {
             event.preventDefault();
+            $(".content-hover").css("display", "none");
             $(".modal-background").css("display", "block");
              });
         $("#cerrar").on("click", function() {
